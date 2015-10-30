4b3980b04df8a728f64fefcd2c059ed00ac96a24
Chris <chris.cornelison@gmail.com>
2015-10-29 19:49:22 -0500

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject match-record "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject match-record "0.1.0"
+  :description "A Clojure library with a match macro `match-record` for the purpose of matching on Clojure record types."
+  :url "https://github.com/chriscnc/match-record"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
